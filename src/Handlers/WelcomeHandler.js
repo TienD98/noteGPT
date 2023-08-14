e35cc564a6e3fda14c762d7038cba5329a283d9f
Tien <tienminh1351@gmail.com>
2023-08-14 01:07:30 -0700

    up

--- src/Handlers/WelcomeHandler.js
+++ src/Handlers/WelcomeHandler.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 function SigninHandler() {
-    axios.get('https://notegpt-686471fdfc45.herokuapp.com/welcome')
+    axios.get('https://notegpt-686471fdfc45.herokuapp.com/welcome', { withCredentials: true })
         .then((res) => {
             console.log(res.data);
         }).catch((err) => {
