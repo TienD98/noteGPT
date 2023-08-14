1afafd92bcf70aac0c800f5f2f93eff060e49a77
Tien <tienminh1351@gmail.com>
2023-08-13 18:45:35 -0700

    welcome page after sign in

---
+++ src/Handlers/WelcomeHandler.js
@@ -0,0 +1,10 @@
+// import axios from "axios";
+// import { useEffect } from "react";
+
+// function SigninHandler() {
+//     useEffect(() => {
+//         axios.get('https://notegpt-686471fdfc45.herokuapp.com/welcome').then((res) => {
+//             res
+//         })
+//     })
+// }
