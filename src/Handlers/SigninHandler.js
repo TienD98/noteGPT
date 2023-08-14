e3efc426b27a0f44c9e8eb27725763940bf97879
Tien <tienminh1351@gmail.com>
2023-08-14 00:56:42 -0700

    update

--- src/Handlers/SigninHandler.js
+++ src/Handlers/SigninHandler.js
@@ -2,17 +2,21 @@ import axios from "axios";
 
 function SigninHandler() {
     const signin = (user, pass, nav) => {
-        axios.post('https://notegpt-686471fdfc45.herokuapp.com/signin', {
-            username: user,
-            password: pass
-        }).then((res) => {
-            document.getElementById('alert').classList.add('visually-hidden');
-            nav('/noteGPT/success');
-            console.log(res);
-        }).catch((err) => {
-            document.getElementById('alert').classList.remove('visually-hidden');
-            console.log(err);
-        })
+        axios.post('https://notegpt-686471fdfc45.herokuapp.com/signin',
+            {
+                username: user,
+                password: pass
+            },
+            { withCredentials: true })
+            .then((res) => {
+                document.getElementById('alert').classList.add('visually-hidden');
+                nav('/noteGPT/success');
+                console.log(res);
+            })
+            .catch((err) => {
+                document.getElementById('alert').classList.remove('visually-hidden');
+                console.log(err);
+            })
 
     }
 
--- src/Handlers/WelcomeHandler.js
+++ src/Handlers/WelcomeHandler.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 function SigninHandler() {
-    axios.get('https://notegpt-686471fdfc45.herokuapp.com/welcome', { withCredentials: true })
+    axios.get('https://notegpt-686471fdfc45.herokuapp.com/welcome')
         .then((res) => {
             console.log(res.data);
         }).catch((err) => {
