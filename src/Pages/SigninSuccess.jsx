bb88ff356975d68fff691729a3bfd0ca1ea69931
Tien <tienminh1351@gmail.com>
2023-08-14 12:20:19 -0700

    update

--- src/Pages/SigninSuccess.jsx
+++ src/Pages/SigninSuccess.jsx
@@ -1,11 +1,33 @@
-import WelcomeHandler from '../Handlers/WelcomeHandler';
+// import WelcomeHandler from '../Handlers/WelcomeHandler';
+import axios from "axios";
 
 function SigninSuccess() {
+    let username = "";
+    let password = "";
+    let hashedPassword = "";
+    const click = () => {
 
+        axios.get('https://notegpt-686471fdfc45.herokuapp.com/welcome', { withCredentials: true })
+            .then((res) => {
+                username = res.data.username;
+                password = res.data.password;
+                hashedPassword = res.data.hashedPassword;
+                console.log(res.data);
+            }).catch((err) => {
+                console.log(err);
+            })
+
+    }
     return (
-        <div>
+        <div id='loader'>
             <h1>WELCOME TO THE APP! </h1>
-            <button className="btn btn-primrary" onClick={() => { WelcomeHandler() }}>Click</button>
+            {/* <button className="btn btn-primrary" onClick={() => { WelcomeHandler() }}>Click</button> */}
+            <button className='btn btn-link' onClick={click}>account</button>
+            <div>
+                <h3>username: {username}</h3>
+                <h3>password: {password}</h3>
+                <h3>hashedPassword: {hashedPassword}</h3>
+            </div>
         </div>
     )
 }
