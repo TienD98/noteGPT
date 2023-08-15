51747885570e6b040146709c5e22cf5a7a6f8ded
Tien <tienminh1351@gmail.com>
2023-08-14 19:52:17 -0700

    user info after success signin

--- src/Pages/SigninSuccess.jsx
+++ src/Pages/SigninSuccess.jsx
@@ -1,32 +1,37 @@
-// import WelcomeHandler from '../Handlers/WelcomeHandler';
 import axios from "axios";
+import { useRef } from "react";
+import { useEffect } from "react";
 
 function SigninSuccess() {
-    let username = "";
-    let password = "";
-    let hashedPassword = "";
-    const click = () => {
+    let user = useRef();
 
+    useEffect(() => {
         axios.get('https://notegpt-686471fdfc45.herokuapp.com/welcome', { withCredentials: true })
             .then((res) => {
-                username = res.data.username;
-                password = res.data.password;
-                hashedPassword = res.data.hashedPassword;
-                console.log(res.data);
+                user.current = res.data;
+                console.log(user.current);
             }).catch((err) => {
                 console.log(err);
-            })
+            });
+    });
 
+    const click = () => {
+        document.getElementById('user').innerHTML = "Username:" + user.current.username;
+        document.getElementById('pass').innerHTML = "Password: " + user.current.password;
+        document.getElementById('hash').innerHTML = "Hashed Password:" + user.current.hashedPassword;
     }
+
     return (
-        <div id='loader'>
+        <div>
             <h1>WELCOME TO THE APP! </h1>
-            {/* <button className="btn btn-primrary" onClick={() => { WelcomeHandler() }}>Click</button> */}
-            <button className='btn btn-link' onClick={click}>account</button>
             <div>
-                <h3>username: {username}</h3>
-                <h3>password: {password}</h3>
-                <h3>hashedPassword: {hashedPassword}</h3>
+                <button className="btn btn-link" onClick={click}>account</button>
+                <button className="btn btn-link" onClick={click}>log out</button>
+            </div>
+            <div className="container">
+                <h3 id="user"></h3>
+                <h3 id="pass"></h3>
+                <h3 id="hash"></h3>
             </div>
         </div>
     )
