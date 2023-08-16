f922c8ac4221ae4150a30d62013cbbe6b85dded9
Tien <tienminh1351@gmail.com>
2023-08-16 14:00:46 -0700

    update bug get user info

--- src/Pages/SigninSuccess.jsx
+++ src/Pages/SigninSuccess.jsx
@@ -10,7 +10,7 @@ function SigninSuccess() {
     useEffect(() => {
         axios.get('https://notegpt-686471fdfc45.herokuapp.com/welcome', { withCredentials: true })
             .then((res) => {
-                user.current = 'hi';
+                console.log(res.data);
             }).catch((err) => {
                 navigate('/signin');
                 console.log(err);
@@ -18,9 +18,15 @@ function SigninSuccess() {
     });
 
     const click = () => {
-        document.getElementById('user').innerHTML = "Username: " + user.current;
-        document.getElementById('pass').innerHTML = "Password: " + user.current;
-        document.getElementById('hash').innerHTML = "Hashed Password: " + user.current;
+        axios.get('https://notegpt-686471fdfc45.herokuapp.com/welcome/profile', { withCredentials: true })
+            .then((result) => {
+                user.current = result.data;
+                document.getElementById('user').innerHTML = "Username: " + user.current.username;
+                document.getElementById('hash').innerHTML = "Hashed Password: " + user.current.password;
+            })
+            .catch((err) => {
+                console.log(err);
+            })
     }
 
     const clickLogout = () => {
@@ -46,7 +52,6 @@ function SigninSuccess() {
                 <div className="card-body">
                     <h5 className="card-title">User info</h5>
                     <p className="card-text lead" id="user"></p>
-                    <p className="card-text lead" id="pass"></p>
                     <p className="card-text lead" id="hash"></p>
                 </div>
             </div>
