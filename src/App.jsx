265694aed23cc0d478137f539745033ca3a8c356
Tien <tienminh1351@gmail.com>
2023-08-30 11:47:06 -0700

    add route main on UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home';
 import Signin from "./Pages/Signin";
 import Register from "./Pages/Register";
+import Main from "./Pages/mainApp";
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route path="signin" element={<Signin />} />
         <Route path="register" element={<Register />} />
+        <Route path="main" element={<Main />} />
       </Routes>
     </Router>
   )
