75933a0d6b8bd72a14d89ed4bf7919cccbcdd316
Tien <tienminh1351@gmail.com>
2023-09-09 15:20:10 -0700

    handle requests for main page

--- src/Handlers/MainAppHandler.js
+++ src/Handlers/MainAppHandler.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 function MainAppHandler() {
+
     function toast(name) {
         document.getElementById(name).classList.remove('hide');
         document.getElementById(name).classList.add('show');
@@ -54,14 +55,52 @@ function MainAppHandler() {
                 if (!res.data.authenticate) {
                     navigate('/signin');
                 }
-                else {
-                    console.log('auth');
-                }
             })
 
     }
 
-    return { toast, mousehold, mouseup, mousemove, clickLogout, checkAuthenticate }
+    function getUsername(user) {
+        axios.get('https://notegpt-686471fdfc45.herokuapp.com/profile/username', { withCredentials: true })
+            .then((res) => {
+                user.current = res.data;
+                document.getElementById('main-username-title').innerHTML = user.current;
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+    }
+
+    function getNote() {
+        axios.get('https://notegpt-686471fdfc45.herokuapp.com/profile/getnote', { withCredentials: true })
+            .then((res) => {
+                // console.log(res.data);
+                const container = document.createElement('div');
+
+                for (let i = 0; i < res.data.length; i++) {
+                    const id = res.data[i].id;
+                    const title = res.data[i].title;
+                    let note = res.data[i].note;
+                    if (note.length >= 40) {
+                        note = note.substring(0, 39) + '...';
+                    }
+                    container.innerHTML = `<a id="` + id + `" onclick="MainAppHandler().toast('toast` + id + `')" class="list-group-item list-group-item-action py-3 lh-tight" href="#/main"><div class="d-flex justify-content-between align-items-center w-100"><strong class="mb-1 titleNote">` + title + `</strong><i class="icon ion-android-delete" id="removeNote"></i></div><div class="col-10 mb-1 small textMain"><p>` + note + `</p></div></a>`
+                    document.getElementById('list-notes').appendChild(container.firstChild);
+                }
+            })
+    }
+
+    async function getNoteArray() {
+        try {
+            let response = await axios.get('https://notegpt-686471fdfc45.herokuapp.com/profile/getnote', { withCredentials: true });
+            return response.data;
+        } catch (error) {
+            console.error("Error fetching notes:", error);
+        }
+    }
+
+
+    return { getNoteArray, getNote, toast, mousehold, mouseup, mousemove, clickLogout, checkAuthenticate, getUsername }
 }
 export default MainAppHandler;
+window.MainAppHandler = MainAppHandler;
 
