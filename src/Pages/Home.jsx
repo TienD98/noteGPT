9ec976198c45f4a1d4b71204fa7bbc4a2b8e7174
Tien <tienminh1351@gmail.com>
2023-09-15 16:21:26 -0700

    update text css on homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -90,7 +90,7 @@ function Home() {
             <section className="about">
                 <div className="about p-2">
                     <h1 className="about-h pt-4">Developer</h1>
-                    <p className="about-p mb-2">Hi there! I'm <span style={{ color: "#66FCF1", fontWeight: "bold" }}>Tien</span>, a recent
+                    <p className="about-p mb-2 fontColor">Hi there! I'm <span style={{ color: "#66FCF1", fontWeight: "bold" }}>Tien</span>, a recent
                         graduate with a Bachelor's degree in Computer Science based in Seattle. Passionate about technology,
                         gaming, and innovation, I founded&nbsp;<span style={{ color: "#66FCF1", fontWeight: "bold" }}>NoteGPT</span> to
                         bridge the gap between communication and creativity.<br /><br />With a goal to expand my coding skills and
@@ -107,7 +107,7 @@ function Home() {
                     <div className="row mb-5">
                         <div className="col-md-8 col-xl-6 text-center mx-auto">
                             <h2 className='link-primary '>Contact us</h2>
-                            <p className="text-center">I'm always eager to hear from the users of <span className='link-primary' style={{ fontFamily: "Aldrich, sans-serif", color: "#1F2833", fontWeight: "bold" }}>NoteGPT</span>. Whether it&#39;s feedback, inquiries, or collaboration opportunities, feel free to reach out. Here&#39;s how you can contact me</p>
+                            <p className="text-center fontColor">I'm always eager to hear from the users of <span className='link-primary' style={{ fontFamily: "Aldrich, sans-serif", color: "#1F2833", fontWeight: "bold" }}>NoteGPT</span>. Whether it&#39;s feedback, inquiries, or collaboration opportunities, feel free to reach out. Here&#39;s how you can contact me</p>
                         </div>
                     </div>
                     <div className="row d-flex justify-content-center">
@@ -118,8 +118,8 @@ function Home() {
                                         <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"></path>
                                     </svg></div>
                                     <div className="px-2">
-                                        <h6 className="mb-0 link-primary">Phone</h6>
-                                        <p className="mb-0">+123456789</p>
+                                        <h6 className="mb-0 link-primary ">Phone</h6>
+                                        <p className="mb-0 fontColor">+123456789</p>
                                     </div>
                                 </div>
                                 <div className="d-flex align-items-center p-3">
@@ -128,7 +128,7 @@ function Home() {
                                     </svg></div>
                                     <div className="px-2">
                                         <h6 className="mb-0 link-primary">Email</h6>
-                                        <p className="mb-0">info@example.com</p>
+                                        <p className="mb-0 fontColor">info@example.com</p>
                                     </div>
                                 </div>
                                 <div className="d-flex align-items-center p-3">
@@ -137,7 +137,7 @@ function Home() {
                                     </svg></div>
                                     <div className="px-2">
                                         <h6 className="mb-0 link-primary">Location</h6>
-                                        <p className="mb-0">123 Example Street</p>
+                                        <p className="mb-0 fontColor">123 Example Street</p>
                                     </div>
                                 </div>
                             </div>
